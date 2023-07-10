804b3e2bbf1a924e0ceb88f864677b48328a4cce
hanan.malka <hanan.malka@ironsrc.com>
2023-07-10 23:31:40 +0300

    Adding code for first action

--- index.js
+++ index.js
@@ -1,20 +1,142 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
+
+const diff = require("diff");
+const axios = require("axios");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
 
-// most @actions toolkit packages have async methods
+const context = github.context;
+const pullRequest = context.payload.pull_request;
+
+const pull_number = pullRequest.number;
+const baseBranch = pullRequest.base.ref;
+
+const slackChannel = core.getInput('slack_channel');
+const reviewers = core.getInput('reviewers');
+const attributes = core.getInput('attributes');
+const slackToken = core.getInput("slack_token");
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+const owner = context.repo.owner;
+const repo = context.repo;
+
+const executeShellCommand = async (command) => {
+  const {stdout} = await exec(`${command}`);
+  return stdout;
+};
+
+const getAttributeChanges = async () => {
+  core.info("Get attribute changes");
+  let greppedValue = ""
+  attributes.forEach((selector, index) => {
+    greppedValue += selector + "=" + (index === attributes.length - 1 ? "" : "|");
+  })
+  const changedLines = await executeShellCommand(
+      `git diff HEAD^ --word-diff | grep -E "${greppedValue}" | grep + | awk '{$1=$1};1'`
+  );
+  core.info(`Attribute changes: \n${changedLines}`)
+  return changedLines.split("\n");
+};
+
+const getOldNewAChangesArray = (gitChanges) => {
+  const changesObjectArray = [];
+  const selectorNames = attributes.join("|");
+  let match;
+  const regexPattern = new RegExp(`\\[-(.*?${selectorNames}.*?)-\\]\\{\\+(.*?${selectorNames}.*?)\\+\\}`, 'g');
+  for (let i = 0; i < gitChanges.length - 1; i += 1) {
+    while ((match = regexPattern.exec(gitChanges[i])) !== null) {
+      const oldValue = match[1];
+      const newValue = match[2];
+      const difference = getDiffBetweenStrings(oldValue, newValue);
+      const changesObject = {
+        old: oldValue,
+        new: newValue,
+        diff: difference
+      }
+      changesObjectArray.push(changesObject);
+    }
+  }
+  return changesObjectArray;
+}
+
+const generateNotificationMessage = async(arrayOfChangedSelectors) => {
+  let selectorsChangesFormatted = "";
+  const separator = '\n-----------------------------------------------------------------------------\n'
+  for (let i = 0; i < arrayOfChangedSelectors.length; i += 1) {
+    const idChangesFormat = "*Origin:* " + arrayOfChangedSelectors[i].old + "\n" +
+        "*New:* " + arrayOfChangedSelectors[i].new + "\n" +
+        "*Diff:* " + arrayOfChangedSelectors[i].diff.action + " *\"" + arrayOfChangedSelectors[i].diff.value
+        + "\"* " + (i === arrayOfChangedSelectors.length - 1 ? "\n" : separator);
+    selectorsChangesFormatted += idChangesFormat;
+  }
+  return ":Warning: The following selectors has been changed:\n\n" +
+      `${selectorsChangesFormatted}\n` +
+      "*Branch*: " + baseBranch + "\n" +
+      "*Service*: " + repo + "\n";
+}
+
+const addReviewersToPullRequest = async (pullRequest) => {
+  const prCurrentReviewers = pullRequest.requested_reviewers;
+  const missingReviewers = reviewers.filter(value => !prCurrentReviewers.includes(value));
+  const githubHeaders = {
+    owner,
+    repo,
+    pull_number,
+    reviewers: missingReviewers
+  }
+
+  const response = await octokit.request("POST /repos/{owner}/{repo}/pulls/{pull_number}/requested_reviewers", githubHeaders);
+  return response.data;
+};
+
+const getDiffBetweenStrings = (oldValue, newValue) => {
+  const difference = {};
+  diff.diffWords(oldValue, newValue).filter((item) => {
+    if (item.added && item.value !== "+") {
+      difference.action = "added";
+      difference.value = item.value;
+    } else if (item.removed && item.value !== "-") {
+      difference.action = "removed";
+      difference.value = item.value;
+    }
+  });
+  return difference;
+}
+
+const sendSlackMessage = async (message) => {
+  await axios.post('https://slack.com/api/chat.postMessage', {
+    channel: slackChannel,
+    text: message
+  }, {
+    headers: {
+      'Authorization': `Bearer ${slackToken}`,
+      'Content-Type': 'application/json'
+    }
+  })
+};
+
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+    const attributeChanges = await getAttributeChanges();
+    let notificationMessage = "";
+    if (attributeChanges) {
+      const arrayOfChangedSelectors = getOldNewAChangesArray(attributeChanges);
+      if (arrayOfChangedSelectors.length !== 0) {
+        notificationMessage = await generateNotificationMessage(arrayOfChangedSelectors);
+        await addReviewersToPullRequest(pullRequest);
+        notificationMessage += notificationMessage +
+            `you were added as a reviewer to the PR: \n` +
+            `https://github.com/${owner}/${repo}/pull/${pull_number}`
+        await sendSlackMessage(notificationMessage);
+      } else {
+        core.info("Selectors format is not supported yet")
+      }
+    } else {
+      core.info("Automation selectors weren't changed");
+    }
+  } catch (ex) {
+    core.info("Failed to detect attributes changes: " + ex);
   }
 }
 
