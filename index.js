e567c5546dfbb3d0d44b2da96b33577de531451b
hanan.malka <hanan.malka@ironsrc.com>
2023-08-01 01:26:23 +0300

    Edit README.md

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ const executeShellCommand = async (command) => {
 
 const getAttributeChanges = async () => {
   core.info(`Get attribute changes: ${attributes}`);
-  core.info(`Branch name: ${branch_name}`);
   let greppedValue = ""
   const attributesArray = JSON.parse(attributes);
   if (!Array.isArray(attributesArray)) {
